ba96c353d9ed81d26e302a33c9c56a851433934a
pyh4382 <64507602+pyh4382@users.noreply.github.com>
2020-06-24 08:05:21 +0900

    [Step 3]

--- 소스.cpp
+++ 소스.cpp
@@ -4,24 +4,32 @@
 #include <string.h> 
 #define NAME_LEN 20
 using namespace std;
+
 class Account {
 private:
-	int accID;
+	const int accID;
 	int balance;
-	char * cusName;
-	long long SSN;
+	char* cusName;
+	const long long SSN;
 public:
-	
-	Account(int accID , int balance ,char* cusName , long long SSN) {
-		this->accID = accID;
+
+
+	Account(int accID, int balance, char* cusName, long long SSN) :accID(accID), SSN(SSN) {
+
 		this->balance = balance;
 		this->cusName = new char[NAME_LEN];
 		strcpy_s(this->cusName, NAME_LEN, cusName);
-		this->SSN = SSN;
-
 	}
+
+	Account(const Account& ref) :accID(ref.accID), SSN(ref.SSN) {
+		balance = ref.balance;
+		this->cusName = new char[NAME_LEN];
+		strcpy_s(this->cusName, NAME_LEN, ref.cusName);
+		cout << "복사생성자" << endl;
+	}
+
 	~Account() {
-		if(cusName != nullptr)
+		if (cusName != nullptr)
 			delete[] cusName;
 	}
 	void accpl(int mon) {
@@ -47,137 +55,138 @@ public:
 	}
 };
 enum eCheckType {
-	ID,SSN
+	ID, SSN
 };
-void accmake();
-void accplus();
-void accmina();
-void printinfo();
 
-template<typename T>
-int check(T a,eCheckType type);
-Account* acclist[MAX_ACC];
-int Max = 0;
-void mainpage() {
-	int selnum;
-	cout << "--------MENU--------" << endl;
-	cout << "   1.계좌개설" << endl;
-	cout << "   2.입     금" << endl;
-	cout << "   3.출     금" << endl;
-	cout << "4.계좌정보 전체 출력" << endl;
-	cout << "   5.종     료" << endl << endl;
-
-	cout << "선택 : ";
-	cin >> selnum;
-	switch (selnum)
-	{
-	case 1: {
-		accmake();
-		break;
-	}
-	case 2: {
-		accplus();
-		break;
-	}
-	case 3: {
-		accmina();
-		break;
-	}
-	case 4: {
-		printinfo();
-		break;
-	}
-	case 5: {
+class AccountManager {
+private:
+	Account* acclist[MAX_ACC];
+	int Max = 0;
+public:
+	~AccountManager() {
 		for (int a = 0; a < Max; a++)
 			delete acclist[a];
+	}
+	void accmake() {
+		int accID;
+		int a;
+		char cusName[NAME_LEN];
+		long long SSN;
+		cout << "[ 계좌 개설 ]" << endl;
+		cout << "계좌 ID : ";
+		cin >> accID;
+		if (check(accID, ID) != -1) {
+			cout << "이미 존재하는 계좌ID입니다" << endl;
+			return;
+		}
+		cout << "이  름 : ";
+		cin >> cusName;
+		cout << "입금액 : ";
+		cin >> a;
+		if (a < 10) {
+			cout << "10원 이상 넣어주세요." << endl;
+			return;
+		}
+		cout << "주민등록 번호 입력(-제외) : ";
+		cin >> SSN;
+		if (check(SSN, eCheckType::SSN) != -1) {
+			cout << "이미 가입된 주민등록번호 입니다" << endl;
+			return;
+		}
+		acclist[Max] = new Account(accID, a, cusName, SSN);
+		Max++;
 		return;
 	}
-	default:
-		cout << "제대로된 수를 선택하세요" << endl;
-		mainpage();
-		break;
+	void accplus() {
+		int accId;
+		int a;
+		cout << "계좌 ID : ";
+		cin >> accId;
+		cout << "입금액 : ";
+		cin >> a;
+		if (check(accId, ID) == -1) {
+			cout << "유효하지 않은 ID입니다." << endl;
+			return;
+		}
+		acclist[check(accId, ID)]->accpl(a);
 	}
-	mainpage();
-}
-void accmake() {
-	int accID;
-	int a;
-	char cusName[NAME_LEN];
-	long long SSN;
-	cout << "[ 계좌 개설 ]" << endl;
-	cout << "계좌 ID : ";
-	cin >> accID;
-	if (check(accID,ID) != -1) {
-		cout << "이미 존재하는 계좌ID입니다" << endl;
-		return;
+	void accmina() {
+		int accId;
+		int a;
+
+		cout << "계좌 ID : ";
+		cin >> accId;
+		cout << "입금액 : ";
+		cin >> a;
+		if (check(accId, ID) == -1) {
+			cout << "유효하지 않은 ID입니다." << endl;
+			return;
+		}
+		bool cright = acclist[check(accId, ID)]->accmin(a);
+		if (cright == false) {
+			cout << "잔액부족" << endl;
+		}
 	}
-	cout << "이  름 : ";
-	cin >> cusName;
-	cout << "입금액 : ";
-	cin >> a;
-	if (a < 10) {
-		cout << "10원 이상 넣어주세요." << endl;
-		return;
+	void printinfo() {
+		for (int i = 0; i < Max; i++) {
+			acclist[i]->print_info();
+		}
 	}
-	cout << "주민등록 번호 입력(-제외) : ";
-	cin >> SSN;
-	if (check(SSN, eCheckType::SSN) != -1) {
-		cout << "이미 가입된 주민등록번호 입니다" << endl;
-		return;
+	template<typename T>
+	int check(T a, eCheckType type) {
+		for (int i = 0; i < Max; i++) {
+
+			if (type == ID)
+				if (acclist[i]->checkID(a))
+					return i;
+			if (type == SSN)
+				if (acclist[i]->checkSSN(a))
+					return i;
+
+		}
+		return -1;
 	}
-	acclist[Max] = new Account(accID,a,cusName,SSN);
-	Max++;
-	return;
-}
-void accplus() {
-	int accId;
-	int a;
-	cout << "계좌 ID : ";
-	cin >> accId;
-	cout << "입금액 : ";
-	cin >> a;
-	if (check(accId,ID) == -1) {
-		cout << "유효하지 않은 ID입니다." << endl;
-		return;
-	}
-	acclist[check(accId,ID)]->accpl(a);
-}
-void accmina() {
-	int accId;
-	int a;
-	
-	cout << "계좌 ID : ";
-	cin >> accId;
-	cout << "입금액 : ";
-	cin >> a;
-	if (check(accId,ID) == -1) {
-		cout << "유효하지 않은 ID입니다." << endl;
-		return;
-	}
-	bool cright = acclist[check(accId,ID)]->accmin(a);
-	if (cright == false) {
-		cout << "잔액부족"<<endl;
-	}
-}
-void printinfo() {
-	for (int i = 0; i < Max; i++) {
-		acclist[i]->print_info();
-	}
-}
-template<typename T>
-int check(T a, eCheckType type) {
-	for (int i = 0; i < Max; i++) {
-		
-		if(type == ID)
-			if (acclist[i]->checkID(a)) 
-				return i;
-		if (type == SSN)
-			if (acclist[i]->checkSSN(a))
-				return i;
-		
-	}
-	return -1;
-}
+};
+
+
 int main() {
-	mainpage();
+	int selnum;
+	AccountManager* manager = new AccountManager();
+	while (true)
+	{
+		cout << "--------MENU--------" << endl;
+		cout << "   1.계좌개설" << endl;
+		cout << "   2.입     금" << endl;
+		cout << "   3.출     금" << endl;
+		cout << "4.계좌정보 전체 출력" << endl;
+		cout << "   5.종     료" << endl << endl;
+
+		cout << "선택 : ";
+		cin >> selnum;
+		switch (selnum)
+		{
+		case 1: {
+			manager->accmake();
+			break;
+		}
+		case 2: {
+			manager->accplus();
+			break;
+		}
+		case 3: {
+			manager->accmina();
+			break;
+		}
+		case 4: {
+			manager->printinfo();
+			break;
+		}
+		case 5: {
+			delete manager;
+			return 0;
+		}
+		default:
+			cout << "제대로된 수를 선택하세요" << endl;
+		}
+	}
 }
